cbd8fdb0bc04800be2903d5796f4f0d8e34e0e6a
Marsyew <yew0024@163.com>
2024-09-23 10:47:24 +0800

    refactor ui

--- src/app/(main)/settings/about/index.tsx
+++ src/app/(main)/settings/about/index.tsx
@@ -1,34 +1,18 @@
 'use client';
 
-import { SiDiscord, SiGithub, SiMedium, SiRss, SiX } from '@icons-pack/react-simple-icons';
 import { Form } from '@lobehub/ui';
-import { Divider } from 'antd';
 import { createStyles } from 'antd-style';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
 import { BRANDING_NAME } from '@/const/branding';
-import {
-  BLOG,
-  DISCORD,
-  EMAIL_BUSINESS,
-  EMAIL_SUPPORT,
-  GITHUB,
-  MEDIDUM,
-  OFFICIAL_SITE,
-  PRIVACY_URL,
-  TERMS_URL,
-  X,
-  mailTo,
-} from '@/const/url';
+
+
 import { useServerConfigStore } from '@/store/serverConfig';
 import { serverConfigSelectors } from '@/store/serverConfig/selectors';
 
-import AboutList from './features/AboutList';
 import Analytics from './features/Analytics';
-import ItemCard from './features/ItemCard';
-import ItemLink from './features/ItemLink';
 import Version from './features/Version';
 
 const useStyles = createStyles(({ css, token }) => ({
@@ -54,9 +38,9 @@ const Page = memo<{ mobile?: boolean }>(({ mobile }) => {
         <Flexbox gap={20} paddingBlock={20} width={'100%'}>
           <div className={styles.title}>{t('version')}</div>
           <Version mobile={mobile} />
-          <Divider style={{ marginBlock: 0 }} />
-          <div className={styles.title}>{t('contact')}</div>
-          <AboutList
+          {/* <Divider style={{ marginBlock: 0 }} /> */}
+          {/* <div className={styles.title}>{t('contact')}</div> */}
+          {/* <AboutList
             ItemRender={ItemLink}
             items={[
               {
@@ -75,10 +59,10 @@ const Page = memo<{ mobile?: boolean }>(({ mobile }) => {
                 value: 'business',
               },
             ]}
-          />
-          <Divider style={{ marginBlock: 0 }} />
-          <div className={styles.title}>{t('information')}</div>
-          <AboutList
+          /> */}
+          {/* <Divider style={{ marginBlock: 0 }} /> */}
+          {/* <div className={styles.title}>{t('information')}</div> */}
+          {/* <AboutList
             ItemRender={ItemCard}
             grid
             items={[
@@ -114,10 +98,10 @@ const Page = memo<{ mobile?: boolean }>(({ mobile }) => {
                 value: 'medium',
               },
             ]}
-          />
-          <Divider style={{ marginBlock: 0 }} />
-          <div className={styles.title}>{t('legal')}</div>
-          <AboutList
+          /> */}
+          {/* <Divider style={{ marginBlock: 0 }} /> */}
+          {/* <div className={styles.title}>{t('legal')}</div> */}
+          {/* <AboutList
             ItemRender={ItemLink}
             items={[
               {
@@ -131,7 +115,7 @@ const Page = memo<{ mobile?: boolean }>(({ mobile }) => {
                 value: 'privacy',
               },
             ]}
-          />
+          /> */}
         </Flexbox>
       </Form.Group>
       {enabledTelemetryChat && <Analytics />}
