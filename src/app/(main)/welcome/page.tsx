6fdebd2ea7f226f80f21e0f1c2502f8f8f3bb0a5
Marsyew <yew0024@163.com>
2024-11-24 19:26:49 +0800

    fix error

--- src/app/(main)/welcome/page.tsx
+++ src/app/(main)/welcome/page.tsx
@@ -1,13 +1,14 @@
-import StructuredData from '@/components/StructuredData';
+// import StructuredData from '@/components/StructuredData';
 import { BRANDING_NAME } from '@/const/branding';
-import { ldModule } from '@/server/ld';
+// import { ldModule } from '@/server/ld';
 import { metadataModule } from '@/server/metadata';
 import { translation } from '@/server/translation';
-import { isMobileDevice } from '@/utils/responsive';
 
-import Actions from './features/Actions';
-import Hero from './features/Hero';
-import Logo from './features/Logo';
+// import { isMobileDevice } from '@/utils/responsive';
+
+// import Actions from './features/Actions';
+// import Hero from './features/Hero';
+// import Logo from './features/Logo';
 
 export const generateMetadata = async () => {
   const { t } = await translation('metadata');
@@ -19,22 +20,23 @@ export const generateMetadata = async () => {
 };
 
 const Page = async () => {
-  const mobile = isMobileDevice();
-  const { t } = await translation('metadata');
-  const ld = ldModule.generate({
-    description: t('welcome.description', { appName: BRANDING_NAME }),
-    title: t('welcome.title', { appName: BRANDING_NAME }),
-    url: '/welcome',
-  });
+  // const mobile = isMobileDevice();
+  // const { t } = await translation('metadata');
+  // const ld = ldModule.generate({
+  //   description: t('welcome.description', { appName: BRANDING_NAME }),
+  //   title: t('welcome.title', { appName: BRANDING_NAME }),
+  //   url: '/welcome',
+  // });
 
-  return (
-    <>
-      <StructuredData ld={ld} />
-      <Logo mobile={mobile} />
-      <Hero />
-      <Actions mobile={mobile} />
-    </>
-  );
+  // return (
+  //   <>
+  //     <StructuredData ld={ld} />
+  //     <Logo mobile={mobile} />
+  //     <Hero />
+  //     <Actions mobile={mobile} />
+  //   </>
+  // );
+  return null;
 };
 
 Page.displayName = 'Welcome';
